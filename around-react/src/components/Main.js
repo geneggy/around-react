ade32f37d200ee85345ed57d02ee70087eb3ee0f
geneggy <geneggy@gmail.com>
2020-08-31 23:45:50 -0700

    began templating popupWithForm

--- around-react/src/components/Main.js
+++ around-react/src/components/Main.js
@@ -2,9 +2,6 @@ import React from 'react';
 import avatar from '../images/Avatar.png';
 import pen from '../images/pen.svg';
 
-
-
-
 function Main() {
   function handleEditAvatarClick(e) {
     e.preventDefault();
@@ -13,21 +10,25 @@ function Main() {
 
   function handleEditProfileClick(e) {
     e.preventDefault();
-    document.querySelector('.popup_edit').classList.add('popup_open')
+    document.querySelector('.popup_edit').classList.add('popup_open');
   }
 
   function handleAddPlaceClick(e) {
     e.preventDefault();
-    document.querySelector('.popup_new-place').classList.add('popup_open')
+    document.querySelector('.popup_new-place').classList.add('popup_open');
   }
-
 
   return (
     <main>
       <section className="profile">
         <div className="profile__avatar-container">
           <img src={avatar} className="profile__avatar" alt="avatar" />
-          <img src={pen} alt="button" className="profile__avatar-edit" onClick={handleEditAvatarClick}/>
+          <img
+            src={pen}
+            alt="button"
+            className="profile__avatar-edit"
+            onClick={handleEditAvatarClick}
+          />
         </div>
 
         <div className="profile__info">
--- around-react/src/components/PopupWithForm.js
+++ around-react/src/components/PopupWithForm.js
@@ -1,9 +1,73 @@
 import React from 'react';
-import logo from '../images/logo.svg';
 
-function PopupWithForm() {
+// props name :edit
+// props title : popup__title
+// props onExit : onClick closing popup
+// props onSubmit: onSubmit submit
+// next step, export this to separate components with specific form fields or
+// write a way to generate unique form fields for each specific popup.  
+
+
+function PopupWithForm(props) {
   return (
-  //
+    <div
+      className={`form popup__${props.name} ${
+        props.isOpen ? 'popup_open' : ''
+      }`}
+    >
+      <div className="popup__container">
+        <button
+          className="popup__exit"
+          aria-label="close"
+          onClick={props.onExit}
+        ></button>
+        <h2 className="popup__title">{props.title}</h2>
+
+        <form
+          className={`popup__form popup__form_${props.name}`}
+          onSubmit={props.onSubmit}
+          novalidate
+        >
+          <input
+            type="text"
+            className="popup__form-name popup__input"
+            id="name-input"
+            value=""
+            name="name"
+            placeholder="name"
+            required
+            minlength="2"
+            maxlength="40"
+          />
+          <span className="popup__error" id="name-input-error">
+            Test
+          </span>
+
+          <input
+            type="text"
+            className="popup__form-about popup__input"
+            id="about-input"
+            value=""
+            name="about"
+            placeholder="about"
+            required
+            minlength="2"
+            maxlength="200"
+          />
+          <span className="popup__error" id="about-input-error">
+            Test
+          </span>
+
+          <button
+            className="popup__button popup__button-edit"
+            aria-label="save"
+            type="submit"
+          >
+            Save
+          </button>
+        </form>
+      </div>
+    </div>
   );
 }
 
